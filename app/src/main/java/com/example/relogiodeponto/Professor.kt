281c1f1cacfc4690164e7fe94b5e5f7c56dafb99
Caroline-estrela <carolineassuncao2019@gmail.com>
2024-10-21 12:54:51 +0100

    merge acepte

--- app/src/main/java/com/example/relogiodeponto/Professor.kt
+++ app/src/main/java/com/example/relogiodeponto/Professor.kt
@@ -1,8 +1,20 @@
 package com.example.relogiodeponto
 
-class Professor (val funcionario: Funcionario, val classelistid: Int){
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
-
-
-
+class Professor : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_professor)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
 }
